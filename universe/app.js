cfbc281ddaf484da0a1b8a43c70caf01cc41289d
ayushsingh953 <ayushsingh35323@gmail.com>
2025-12-18 01:16:49 +0530

    fixed redis error

--- universe/app.js
+++ universe/app.js
@@ -16,9 +16,9 @@ const io = socketIo(server, {
 const Redis = require("ioredis");
 const redis = new Redis({
   host: process.env.REDIS_HOST || "redis",
-  port: process.env.REDIS_PORT || 6379,
+  port: 6379,
 });
-
+ 
 redis.on("connect", () => console.log("✅ Connected to Redis!"));
 redis.on("error", (err) => {
   console.error("🚨 Redis Error:", err.message);
