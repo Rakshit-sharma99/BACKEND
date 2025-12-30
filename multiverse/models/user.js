f405c0ee01ea124e6bf02b5b99f19c4bad383eef
ayushsingh953 <ayushsingh35323@gmail.com>
2025-12-30 17:09:51 +0530

    removed the reg required field from multiverse user model

--- multiverse/models/user.js
+++ multiverse/models/user.js
@@ -17,7 +17,6 @@ const userSchema = new mongoose.Schema(
     },
     reg: {
       type: Number,
-      required: [true, "Please provide the registration number."],
     },
     course: {
       type: String,
