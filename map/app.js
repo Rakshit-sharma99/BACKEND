2f92d9e9fe5d780cc3b54a334804d1ca26998154
ayushsingh953 <ayushsingh35323@gmail.com>
2026-01-11 10:51:32 +0530

    map service initial stage

---
+++ map/app.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const helmet = require("helmet");
+const http = require("http");
+const connectDB = require("./db/connect");
+
+const app = express();
+const server = http.createServer(app);
+
+app.set("trust proxy", 1);
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(
+    `[${new Date().toISOString()}] ${req.ip} ${req.method} ${req.originalUrl}`
+  );
+  next();
+});
+
+app.get("/map/api/v1/hello", (req, res) => {
+  res.send("Map service responding!");
+});
+
+const port = process.env.PORT || 7040;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    server.listen(port, () => {
+      console.log(`✅ Server is listening to port ${port}.`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
