c32ef790d936821bcd08d0d5c41de729786ae5c7
ayushsingh953 <ayushsingh35323@gmail.com>
2026-01-14 17:15:36 +0530

    handling tags only if tags are present

--- memory/config/event_handlers/create_memory.js
+++ memory/config/event_handlers/create_memory.js
@@ -5,7 +5,9 @@ const create_memory = async (messageValue) => {
   try {
     const data = JSON.parse(messageValue);
     const memory = await Memory.create(data.memoryData);
-    await handleTags({ tags:data.memoryData.tags, memoryId: memory._id, userId:data.memoryData.createdBy,callSign:data.memoryData.callSign });
+    if(data.memoryData.tags){
+      await handleTags({ tags:data.memoryData.tags, memoryId: memory._id, userId:data.memoryData.createdBy,callSign:data.memoryData.callSign });
+    }
     console.log("📩 Successfully processed create memory topic");
   } catch (error) {
     console.log(error);
