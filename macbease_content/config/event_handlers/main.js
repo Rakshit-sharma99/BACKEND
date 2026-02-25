d4c324a20c1c0da3b2c0b3464c4e5b62a7df6164
ayushsingh953 <ayushsingh35323@gmail.com>
2026-02-25 21:12:13 +0530

    removed macbease_content service

--- macbease_content/config/event_handlers/main.js
+++
@@ -1,9 +0,0 @@
-const { update_macbease_content } = require("./universe_event_handlers/update_macbease_content");
-
-const prefix = process.env.KAFKA_CLIENT_ID;
-
-const handlers = {
-  [`${prefix}_update_macbease_content`]: update_macbease_content,
-};
-
-module.exports = { handlers };
