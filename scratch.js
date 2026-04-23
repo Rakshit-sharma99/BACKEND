bc82c258bed6cca6cd188137afe7344bb71fe7c0
spargens <amartyasingh1010@gmail.com>
2026-04-24 00:55:27 +0530

    physical ticket copy implemented

--- scratch.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Event = require('./event/models/Event');
-require('dotenv').config({ path: './env/event/.env' });
-
-mongoose.connect(process.env.MONGO_URI).then(async () => {
-  const event = await Event.findOne({}).lean();
-  console.log("Event URL is:", event.url);
-  process.exit(0);
-});
